7c54ff36d8d48e6c781d6d336d5b295e775a3380
Akash Singh <88923478+AkashSingh2002@users.noreply.github.com>
2023-10-22 19:55:24 +0530

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+
+def home(request):
+    if request == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request,user)
+            return render(request, 'home.html')
+        else:
+            messages.error(request, "Please Register first")
+            return redirect('register')
+    return render(request,'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['firstname']
+        lname = request.POST['lastname']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        myuser = User.objects.create_user(username, fname, lname, phone, email, password)
+        myuser.save()
+
+        messages.success(request, "Your Account has been successfully created.")
+
+        return redirect('home')
+    return render(request, 'register.html')
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
+    return redirect('home')
